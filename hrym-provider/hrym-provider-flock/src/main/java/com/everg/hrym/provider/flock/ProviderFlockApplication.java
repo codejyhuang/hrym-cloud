36938bc3f0a2a4a56f8141540d2820f8cceb1610
apple <jyhuang@everglamming.com>
2019-04-22 12:59:25 +0800

    修改

--- hrym-provider/hrym-provider-flock/src/main/java/com/everg/hrym/provider/flock/ProviderFlockApplication.java
+++ hrym-provider/hrym-provider-flock/src/main/java/com/everg/hrym/provider/flock/ProviderFlockApplication.java
@@ -4,6 +4,7 @@ import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages={"com.everg.hrym"})
 @EnableFeignClients(basePackages = "com.everg.hrym.api")
 @EnableDistributedTransaction
+@RefreshScope
 public class ProviderFlockApplication {
 
     public static void main(String[] args){
